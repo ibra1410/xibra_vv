d75be803caeb96a06c96836711f49989072b4eeb
ibra1410 <160061682+ibra1410@users.noreply.github.com>
2024-11-24 12:51:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    name='xibra_v',  # Name of the library
-    version='0.1',
-    packages=find_packages(),  # Automatically find packages
-    install_requires=[],  # If there are any external dependencies, list them here
-    description='A simple encryption library for text and files with customizable layers of encryption.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='ibrahim',
-    author_email='ibraus34@gmail.com',
-    url='https://github.com/ibra1410/xibra_vv',  # URL of the GitHub repository
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    name="xibra_v",  # اسم مكتبتك
+    version="0.1",  # الإصدار الأول
+    packages=find_packages(),
+    install_requires=[
+        'pyfiglet',
+        'colorama',
+        'twine',
+        'setuptools',
+        'wheel'
+    ],  # أضف المكتبات التي تعتمد عليها هنا
+    author="ibra1410",  # اسم المؤلف
+    author_email="ibraus34@gmail.com",  # بريدك الإلكتروني
+    description="A simple encryption library",
+    long_description=open("README.md").read(),  # وصف المكتبة الكامل
+    long_description_content_type="text/markdown",  # نوع المحتوى
+    url="https://github.com/ibra1410/xibra_v",  # رابط المستودع على GitHub
+    classifiers=[  # التصنيفات الخاصة بمكتبتك على PyPI
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Supported Python versions
 )
